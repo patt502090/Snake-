1e8e8549c6cde0e93bfac128af5861ff1ff1948c
Phodcharaphon <patt502090@hotmail.com>
2024-02-13 22:19:00 +0700

    feat: SnakeHead and Fruit

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
 
+class SnakeHead(Widget):
+    pass
+
+class Fruit(Widget):
+    pass
 class SnakePlusPlusApp(App):
     def build(self):
         Window.size = (900, 600)
@@ -10,5 +16,3 @@ class SnakePlusPlusApp(App):
     
 if __name__== '__main__':
     SnakePlusPlusApp().run()
-
-   
