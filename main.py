a294c27eb89c3b525932fed772766d654b194608
กรวิทย์ กอหลัง <kornwit6404@gmail.com>
2024-02-13 22:06:36 +0700

    edit window size

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import kivy
-
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.core.window import Window
+
 class SnakePlusPlusApp(App):
     def build(self):
+        Window.size = (900, 600)
         return Label(text='Hello, world!')
+    
 if __name__== '__main__':
-        SnakePlusPlusApp().run()
+    SnakePlusPlusApp().run()
+
+   
